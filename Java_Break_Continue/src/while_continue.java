83895d462ed35508c36ec0c755293eaa68ecf828
sachinkumarg35 <90327405+sachinkumarg35@users.noreply.github.com>
2022-07-09 07:54:42 +0530

    Add files via upload

---
+++ Java_Break_Continue/src/while_continue.java
@@ -0,0 +1,17 @@
+
+public class while_continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		while (i < 10) {
+			if (i == 4) {
+				i++;
+				continue;
+			}
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
