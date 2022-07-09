18be226a2ffa88ac1307d3e49d470ef9b912c309
sachinkumarg35 <90327405+sachinkumarg35@users.noreply.github.com>
2022-07-09 07:54:17 +0530

    Add files via upload

---
+++ Java_Boolean/src/Java_boolean.java
@@ -0,0 +1,18 @@
+
+public class Java_boolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isJavaFun = true;
+		boolean isFishtasty = false;
+		int x = 10;
+		int y = 9;
+		System.out.println(isJavaFun);
+		System.out.println(isFishtasty);
+		System.out.println(x > y); // returns true, because 10 is higher than 9. 
+		System.out.println(10 > 9); //returns true, because 10 is higher than 9.
+		System.out.println(x==10); //returns true, because the value of x is equal to 10.
+		System.out.println(15==10); //return false, because the value of 15 is greater than 10.
+	}
+
+}
